c3ec5fdf3d10ea2a72adbcdaa7267d54ea0d6ce4
MarshalGW <136970742+Forever-proglab@users.noreply.github.com>
2024-08-01 14:30:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import telebot
-
+import random
+n = 1
 bot = telebot.TeleBot('7391308423:AAHD2HdQEgJSPrdz3FN3mjp2fuALawB-G8A')
 @bot.message_handler(commands=['start', 'hello'])
 def main(message):
-    bot.send_message(message.chat.id, 'Доброго времени суток')
+    bot.send_message(message.chat.id, f'Доброго времени суток, {message.from_user.first_name}')
+@bot.message_handler(commands=['id'])
+def main(message):
+    bot.send_message(message.chat.id, f'Это ваш id, основное средство для отличия аккаунтов - {message.from_user.id}')
+    bot.send_message(message.chat.id, f'Это ваше имя пользователя, второе средство для отличия аккаунтов после id - @{message.from_user.username}')
 @bot.message_handler(commands=['minfo'])
-def minfo(message):
+def main(message):
     bot.send_message(message.from_user.id, message)
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    bot.send_message(message.from_user.id, message.text)
+@bot.message_handler()
+def hello(message):
+    if message.text.lower() == 'Здравствуйте' or 'Привет' or 'Доброго времени суток':
+        n = random.randint(1,4)
+        if n == 1:
+            bot.send_message(message.chat.id, f'Доброго времени суток, @{message.from_user.username}')
+        elif n == 2:
+            bot.send_message(message.chat.id, f'Доброго времени суток, {message.from_user.first_name}')
+        elif n == 3:
+            bot.send_message(message.chat.id, f'Здравствуйте, @{message.from_user.username}')
+        elif n == 4:
+            bot.send_message(message.chat.id, f'Здравствуйте, {message.from_user.first_name}')
 bot.infinity_polling()
